e6035258f6b3a93785dc4140cd99261bd9eab1af
AlexandraSouthern <southerna@macstudent.com.au>
2023-03-07 18:05:57 +1100

    Initial Commit

---
+++ My Swimming Life/My_Swimming_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Swimming_LifeApp.swift
+//  My Swimming Life
+//
+//  Created by Alexandra Southern on 7/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_Swimming_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
